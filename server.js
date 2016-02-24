21c318a57aa95d283828704c558dfd1a90b95188
Ashish Pathak <ashishpat@users.noreply.github.com>
2016-02-24 15:48:17 -0500

    Update to RSS

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/lfcfeeds', function (req, res) {
         if (err) {
             return console.err(err);
         }
-        res.send(data);
+        res.send(parseString);
     });
 });
 
